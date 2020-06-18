60e004d71dcc43579a81b893c3dfc17806c76242
joeffrey_lamy_3006 <joeffreylamy@hotmail.com>
2020-06-18 22:42:22 +0100

    Refactors notes deleter spec
    why?
    - just clarifying what belongs together

--- spec/requests/note/delete_spec.rb
+++ spec/requests/note/delete_spec.rb
@@ -14,14 +14,28 @@ RSpec.describe 'deleting notes' do
   end
 
   context 'title and notebook specified' do
-    it 'deletes the note' do
-      FakeFS.with_fresh do
-        create_workspace
-        allow(STDIN).to receive(:gets).and_return('y')
-        subject.create_note('new_notebook', ['test_note'])
+    context 'notebook exists' do
+      it 'deletes the note' do
+        FakeFS.with_fresh do
+          create_workspace
+          allow(STDIN).to receive(:gets).and_return('y')
+          subject.create_note('new_notebook', ['test_note'])
 
-        expect { subject.delete_note('new_notebook', %w[test note]) }
-          .to change { File.file?('new_notebook/test_note.md') }.from(true).to(false)
+          expect { subject.delete_note('new_notebook', %w[test note]) }
+            .to change { File.file?('new_notebook/test_note.md') }.from(true).to(false)
+        end
+      end
+    end
+
+    context 'notebook does not exist' do
+      it 'raises an error' do
+        FakeFS.with_fresh do
+          create_workspace
+          allow(STDIN).to receive(:gets).and_return('n')
+
+          expect { subject.delete_note('invalid_notebook', %w[test note]) }
+            .to raise_error(ArgumentError, 'notebook does not exist')
+        end
       end
     end
   end
@@ -46,18 +60,6 @@ RSpec.describe 'deleting notes' do
 
         expect { subject.delete_note('', %w[test note]) }
           .to raise_error(ArgumentError, 'no notebook specified')
-      end
-    end
-  end
-
-  context 'notebook does not exist' do
-    it 'raises an error' do
-      FakeFS.with_fresh do
-        create_workspace
-        allow(STDIN).to receive(:gets).and_return('n')
-
-        expect { subject.delete_note('invalid_notebook', %w[test note]) }
-          .to raise_error(ArgumentError, 'notebook does not exist')
       end
     end
   end
